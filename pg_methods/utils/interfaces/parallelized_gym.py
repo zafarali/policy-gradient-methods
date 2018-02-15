f6b083aaab037f4a85ee28949b192230fd814dcf
zafarali <zafarali.ahmed@gmail.com>
2018-02-14 22:15:03 -0500

    Add all_done checker

--- pg_methods/utils/interfaces/parallelized_gym.py
+++ pg_methods/utils/interfaces/parallelized_gym.py
@@ -210,6 +210,10 @@ class SubprocVecEnv(VecEnv):
     def num_envs(self):
         return len(self.remotes)
 
+    @property
+    def all_done(self):
+        return self.n_envs == sum(self.dones)
+
 def make_env(env_id, seed, rank, logger=None):
     logging.warn('Atari is not supported. See https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/envs.py for atari support')
     def _thunk():
